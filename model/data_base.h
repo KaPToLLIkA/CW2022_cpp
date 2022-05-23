d18c116fd7d55676610c600ae5a7c465025e0698
hexagony <eduardpur18@gmail.com>
2022-05-23 23:40:27 +0300

    base database ended

--- model/data_base.h
+++ model/data_base.h
@@ -1,11 +1,39 @@
 #ifndef DATA_BASE_H
 #define DATA_BASE_H
 
+#include "../slow_list.h"
+#include "office.h"
 
 class data_base
 {
+    static data_base *_i;
+
+    slow_list<office> offices;
+
+    explicit data_base();
 public:
-    data_base();
+    static data_base* i() {
+        if (_i == nullptr) {
+            _i = new data_base();
+        }
+        return _i;
+    }
+
+    void add_office(office* office) {
+        offices.add(office);
+    }
+
+    void remove_office(office* office) {
+        offices.remove_by_value(office);
+    }
+
+    uint32_t offices_count() {
+        return offices.lenght();
+    }
+
+    office* get_office_at(uint32_t index) {
+        return offices.get_value_at(index);
+    }
 };
 
 #endif // DATA_BASE_H
